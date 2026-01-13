222f75c6e029a29886839fbf1d31c0adeff45eb8
cornellj834-lang <cornellj834@gmail.com>
2026-01-13 15:49:55 -0500

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,23 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig({
-  plugins: [react()],
-  define: {
-    'process.env': process.env
-  },
-  build: {
-    outDir: 'dist',
-    rollupOptions: {
-      input: {
-        main: './index.html'
+export default defineConfig(({ mode }) => {
+  // Load env file based on `mode` in the current working directory.
+  // Set the third parameter to '' to load all env regardless of the `VITE_` prefix.
+  // Cast process to any to avoid TS error: Property 'cwd' does not exist on type 'Process'
+  const env = loadEnv(mode, (process as any).cwd(), '');
+  return {
+    plugins: [react()],
+    define: {
+      'process.env.API_KEY': JSON.stringify(env.API_KEY)
+    },
+    build: {
+      outDir: 'dist',
+      rollupOptions: {
+        input: {
+          main: './index.html'
+        }
       }
     }
-  }
+  };
 });
